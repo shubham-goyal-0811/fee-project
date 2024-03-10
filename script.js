33304001da5f0e62633d17b51cde4006e5efd199
Shubham Goyal <122973606+shubham-goyal-0811@users.noreply.github.com>
2024-03-10 21:39:09 +0530

    files uploaded

---
+++ script.js
@@ -0,0 +1,123 @@
+document.addEventListener('scroll', function () {
+    const scrollPosition = window.scrollY;
+    const parallaxItems = document.querySelectorAll('.parallax-item');
+    const parallaxItemBottom = document.querySelector('.parallax-item-bottom');
+    const parallaxItemTop = document.querySelector('.parallax-item-top');
+
+    parallaxItems.forEach(function (item, index) {
+        const speed = (index + 1) * 1;
+        item.style.transform = `translateX(${scrollPosition * speed}px)`;
+    });
+
+    parallaxItemBottom.style.transform = `translateY(-${scrollPosition * 0.3}px)`;
+    parallaxItemTop.style.transform = `translateX(-${scrollPosition * 0.3}px)`;
+});
+
+const toggleButton = document.querySelector('.toggle-button');
+const navLinks = document.querySelector('.nav-links');
+
+toggleButton.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
+//APIKEY: 
+// Titles: https://omdbapi.com/?s=thor&page=1&apikey=fc1fef96
+// details: http://www.omdbapi.com/?i=tt3896198&apikey=b00e95a6
+
+// ... existing code ...
+
+const movieSearchBox = document.getElementById('movie-search-box');
+const searchList = document.getElementById('search-list');
+const resultGrid = document.getElementById('result-grid');
+
+// ... existing code ...
+
+
+
+
+// load movies from API
+async function loadMovies(searchTerm){
+    const URL = `https://omdbapi.com/?s=${searchTerm}&page=1&apikey=b00e95a6`;
+    const res = await fetch(`${URL}`);
+    const data = await res.json();
+    // console.log(data.Search);
+    if(data.Response == "True") displayMovieList(data.Search);
+}
+
+function findMovies(){
+    let searchTerm = (movieSearchBox.value).trim();
+    if(searchTerm.length > 0){
+        searchList.classList.remove('hide-search-list');
+        loadMovies(searchTerm);
+    } else {
+        searchList.classList.add('hide-search-list');
+    }
+}
+
+function displayMovieList(movies){
+    searchList.innerHTML = "";
+    for(let idx = 0; idx < movies.length; idx++){
+        let movieListItem = document.createElement('div');
+        movieListItem.dataset.id = movies[idx].imdbID; // setting movie id in  data-id
+        movieListItem.classList.add('search-list-item');
+        if(movies[idx].Poster != "N/A")
+            moviePoster = movies[idx].Poster;
+        else 
+            moviePoster = "images/image_not_found.png";
+
+        movieListItem.innerHTML = `
+        <div class = "search-item-thumbnail">
+            <img src = "${moviePoster}">
+        </div>
+        <div class = "search-item-info">
+            <h3>${movies[idx].Title}</h3>
+            <p>${movies[idx].Year}</p>
+        </div>
+        `;
+        searchList.appendChild(movieListItem);
+    }
+    loadMovieDetails();
+}
+
+function loadMovieDetails(){
+    const searchListMovies = searchList.querySelectorAll('.search-list-item');
+    searchListMovies.forEach(movie => {
+        movie.addEventListener('click', async () => {
+            // console.log(movie.dataset.id);
+            searchList.classList.add('hide-search-list');
+            movieSearchBox.value = "";
+            const result = await fetch(`http://www.omdbapi.com/?i=${movie.dataset.id}&apikey=b00e95a6`);
+            const movieDetails = await result.json();
+            // console.log(movieDetails);
+            displayMovieDetails(movieDetails);
+        });
+    });
+}
+
+function displayMovieDetails(details){
+    resultGrid.innerHTML = `
+    <div class = "movie-poster">
+        <img src = "${(details.Poster != "N/A") ? details.Poster : "images/image_not_found.png"}" alt = "movie poster">
+    </div>
+    <div class = "movie-info">
+        <h3 class = "movie-title">${details.Title}</h3>
+        <ul class = "movie-misc-info">
+            <li class = "year">Year: ${details.Year}</li>
+            <li class = "rated">Ratings: ${details.Rated}</li>
+            <li class = "released">Released: ${details.Released}</li>
+        </ul>
+        <p class = "genre"><b>Genre:</b> ${details.Genre}</p>
+        <p class = "writer"><b>Writer:</b> ${details.Writer}</p>
+        <p class = "actors"><b>Actors: </b>${details.Actors}</p>
+        <p class = "plot"><b>Plot:</b> ${details.Plot}</p>
+        <p class = "language"><b>Language:</b> ${details.Language}</p>
+        <p class = "awards"><b><i class = "fas fa-award"></i></b> ${details.Awards}</p>
+    </div>
+    `;
+    document.getElementById('result-show').scrollIntoView();
+}
+
+window.addEventListener('click', (event) => {
+    if (event.target.className != "form-control") {
+        searchList.classList.add('hide-search-list');
+    }
+});
